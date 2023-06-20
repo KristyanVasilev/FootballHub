aa4c7ec3a2e355e64b252e20895aad9de4d4d5a4
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-06-20 13:44:20 +0300

    docker preparation files

---
+++ marek-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<div>test sadasdadasd</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
